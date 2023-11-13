a386d6c2c5d1f07c90ec3d0ab5622f64a115f8cb
acly7946 <66972514+acly7946@users.noreply.github.com.>
2023-11-13 18:02:01 +0000

    Add constructor

---
+++ src/bcu/cmp5332/librarysystem/commands/ShowPatron.java
@@ -0,0 +1,22 @@
+package bcu.cmp5332.librarysystem.commands;
+
+import bcu.cmp5332.librarysystem.model.Book;
+import bcu.cmp5332.librarysystem.model.Library;
+import bcu.cmp5332.librarysystem.model.Patron;
+import bcu.cmp5332.librarysystem.main.LibraryException;
+
+import java.time.LocalDate;
+
+public class ShowPatron implements Command {
+
+	private final int patronId;
+
+	public ShowPatron(int patronId) {
+		this.patronId = patronId;
+	}
+
+    @Override
+    public void execute(Library library, LocalDate currentDate) throws LibraryException {
+
+    }
+}
