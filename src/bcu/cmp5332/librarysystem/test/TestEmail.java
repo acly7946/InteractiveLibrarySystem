14452f1f5c924f1c0d45bdd68bf90c2ea868cddf
acly7946 <66972514+acly7946@users.noreply.github.com.>
2023-11-17 13:54:11 +0000

    Test getter/setter methods

--- src/bcu/cmp5332/librarysystem/test/TestEmail.java
+++ src/bcu/cmp5332/librarysystem/test/TestEmail.java
@@ -1,7 +1,27 @@
 package bcu.cmp5332.librarysystem.test;
 
+import static org.junit.Assert.assertEquals;
+
+
+import bcu.cmp5332.librarysystem.main.LibraryException;
+import bcu.cmp5332.librarysystem.model.Patron;
+
+import java.io.IOException;
 import org.junit.Test;
 
 public class TestEmail {
+
+	Patron patron = new Patron(-1, "Test", "123", "Email@Email.com");
+
+	@Test
+	public void testGetEmail() throws LibraryException, IOException {
+		assertEquals(patron.getEmail(), "Email@Email.com");
+	}
+
+	@Test
+	public void testSetEmail() throws LibraryException, IOException {
+		patron.setEmail("NEWMAIL@NEWMAIL.com");
+		assertEquals(patron.getEmail(), "NEWMAIL@NEWMAIL.com");
+	}
 
 }
