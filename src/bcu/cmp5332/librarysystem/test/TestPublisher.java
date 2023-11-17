ceaee2046107e580d88a0f0c077dffbac126b5a2
acly7946 <66972514+acly7946@users.noreply.github.com.>
2023-11-17 14:05:24 +0000

    Test getters/setters

--- src/bcu/cmp5332/librarysystem/test/TestPublisher.java
+++ src/bcu/cmp5332/librarysystem/test/TestPublisher.java
@@ -1,7 +1,24 @@
 package bcu.cmp5332.librarysystem.test;
 
+import bcu.cmp5332.librarysystem.model.Book;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-class TestPublisher {
+public class TestPublisher {
+
+	Book book = new Book(-1, "Test", "Test", "PUBLISHER", "Year");
+
+	@Test
+	public void testGetPublisher() {
+		assertEquals(book.getPublisher(), "PUBLISHER");
+	}
+
+	@Test
+	public void testSetPublisher() {
+		book.setPublisher("NEWPUBLISHER");
+		assertEquals(book.getPublisher(), "NEWPUBLISHER");
+	}
 
 }
