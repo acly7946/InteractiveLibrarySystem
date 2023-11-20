d9df2a03f758759c28d7908bf374baef55a9a8f1
acly7946 <66972514+acly7946@users.noreply.github.com.>
2023-11-20 15:41:50 +0000

    Add test for validating saved patron data

--- src/bcu/cmp5332/librarysystem/test/TestEmail.java
+++ src/bcu/cmp5332/librarysystem/test/TestEmail.java
@@ -5,7 +5,11 @@ import static org.junit.Assert.assertEquals;
 import bcu.cmp5332.librarysystem.main.LibraryException;
 import bcu.cmp5332.librarysystem.model.Patron;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class TestEmail {
@@ -21,6 +25,20 @@ public class TestEmail {
 	public void testSetEmail() throws LibraryException, IOException {
 		patron.setEmail("NEWMAIL@NEWMAIL.com");
 		assertEquals(patron.getEmail(), "NEWMAIL@NEWMAIL.com");
+	}
+
+	@Test
+	public void testSavedFormat() throws FileNotFoundException, LibraryException {
+		final String RESOURCE = "./resources/data/patrons.txt";
+		final String SEPARATOR = "::";
+
+		try (Scanner sc = new Scanner(new File(RESOURCE))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] properties = line.split(SEPARATOR, -1);
+				assertEquals(5, properties.length);
+            }
+        }
 	}
 
 }
