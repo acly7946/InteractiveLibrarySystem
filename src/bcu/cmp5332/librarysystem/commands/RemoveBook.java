f6e6281e3c51c9f6c2b691796b16373d2e61ed8c
acly7946 <66972514+acly7946@users.noreply.github.com.>
2023-12-06 18:05:54 +0000

    Add Removebook command

---
+++ src/bcu/cmp5332/librarysystem/commands/RemoveBook.java
@@ -0,0 +1,20 @@
+package bcu.cmp5332.librarysystem.commands;
+
+import bcu.cmp5332.librarysystem.model.Library;
+import bcu.cmp5332.librarysystem.main.LibraryException;
+
+import java.time.LocalDate;
+
+public class RemoveBook implements Command {
+
+	private final int bookId;
+
+	public RemoveBook(int bookId) {
+		this.bookId = bookId;
+	}
+
+    @Override
+    public void execute(Library library, LocalDate currentDate) throws LibraryException {
+		library.removeBook(bookId);
+    }
+}
