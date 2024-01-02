99b6afbbf1170ae5cd5c47f99eb195d72610cf56
acly7946 <66972514+acly7946@users.noreply.github.com.>
2024-01-02 13:25:03 +0000

    javadoc.

--- src/bcu/cmp5332/librarysystem/data/LoanDataManager.java
+++ src/bcu/cmp5332/librarysystem/data/LoanDataManager.java
@@ -13,10 +13,78 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.Scanner;
 
+/**
+ * LoanDataManager class is used to {@link loadData load} and
+ * {@link storeData store}
+ * {@link Loan} data using the text file
+ * storage.
+ *
+ * <p>
+ * It is an implementation of the {@link DataManager} interface. It has a
+ * RESOURCE field
+ * that holds the path to the text file (loans.txt) that will be used as storage
+ * for the Loan data.
+ * </p>
+ *
+ * <p>
+ * The {@link loadData loadData(Library)} method loads data from loans.txt file
+ * to the library
+ * and the {@link storeData storeData(Library)} method stores the loan data that
+ * exist in
+ * the
+ * library to loans.txt file.
+ * </p>
+ *
+ * @see loadData loadData(Library)
+ * @see storeData storeData(Library)
+ * @see RESOURCE
+ * @see DataManager
+ * @see Library
+ * @see Loan
+ */
 public class LoanDataManager implements DataManager {
 
+	/**
+	 * The path to loans.txt file that is used as storage for loan data.
+	 */
 	public final String RESOURCE = "./resources/data/loans.txt";
 
+	/**
+	 *
+	 * Load data from the file storage to the library.
+	 *
+	 * <p>
+	 * The method opens the file that holds the loan data using the RESOURCE field
+	 * and creates a Scanner object to read it. Then it reads each line and splits
+	 * it in parts using the predefined {@link DataManager#SEPARATOR} (::). The
+	 * result is an
+	 * array of strings with each of the strings representing a different
+	 * {@link Loan}
+	 * property. Then the function parses each of the string parts to its
+	 * corresponding property (the properties are {@link storeData stored} in a
+	 * specific predefined
+	 * order) of a {@link Loan} object. Finally the new Loan is added to the
+	 * corresponding
+	 * Book and the Book is added to the corresponding Patron's book list. If the
+	 * patron id can't be parsed to an integer a LibraryException is thrown.
+	 * </p>
+	 *
+	 * @param library the library object where the data will be loaded to
+	 * @throws IOException      if the file is not found or can't be read.
+	 * @throws LibraryException if the id of the loan that is being read can't be
+	 *                          parsed to an integer or if any of the
+	 *                          Loan.startDate or Loan.dueDate can't be parsed to a
+	 *                          LocalDate
+	 * @see RESOURCE
+	 * @see Scanner
+	 * @see String#split(String, int)
+	 * @see DataManager#SEPARATOR
+	 * @see storeData storeData(Library)
+	 * @see Loan
+	 * @see Book#setLoan(Loan)
+	 * @see Patron#addBook(Book)
+	 * @see LocalDate#parse(CharSequence)
+	 */
 	@Override
 	public void loadData(Library library) throws IOException, LibraryException {
 		try (Scanner sc = new Scanner(new File(RESOURCE))) {
@@ -37,17 +105,38 @@ public class LoanDataManager implements DataManager {
 					patron.addBook(book);
 				} catch (NumberFormatException ex) {
 					throw new LibraryException("Unable to parse loan on line " + line_idx
-						+ "\nError: " + ex);
+							+ "\nError: " + ex);
 				}
 				line_idx++;
 			}
 		}
 	}
 
+	/**
+	 * Store data from the library to the text file storage.
+	 *
+	 * The method creates a new FileWriter object using the RESOURCE field and then
+	 * creates a PrintWriter that will be used to write characters to the
+	 * FileWriter. The PrintWriter is initialised using the try-with-resources
+	 * statement to take care of properly closing the resources. Then for each loan
+	 * that is found on each of the patrons book list, a new line is written to the
+	 * file containing the Loan properties in a specific order. After each property,
+	 * the predefined DataManager.SEPARATOR is added.
+	 *
+	 * @param library the library object whose data will be stored in the file
+	 * 			  storage
+	 * @throws java.io.IOException if the file is corrupt or can't be written
+	 * @see RESOURCE
+	 * @see FileWriter
+	 * @see PrintWriter
+	 * @see Patron.getBooks()
+	 * @see DataManager#SEPARATOR
+	 *
+	 */
 	@Override
 	public void storeData(Library library) throws IOException {
-        try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))) {
-            for (Book book : library.getBooks()) {
+		try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))) {
+			for (Book book : library.getBooks()) {
 				if (book.isOnLoan()) {
 					Loan loan = book.getLoan();
 					out.print(book.getId() + SEPARATOR);
@@ -57,8 +146,8 @@ public class LoanDataManager implements DataManager {
 					out.print(loan.getReturnDate().toString() + SEPARATOR);
 					out.println();
 				}
-            }
-        }
+			}
+		}
 	}
 
 }
