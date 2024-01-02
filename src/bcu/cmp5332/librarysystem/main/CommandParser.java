97266a954ca5a5ddd967b8e47a0beca19550750b
acly7946 <66972514+acly7946@users.noreply.github.com.>
2024-01-02 13:31:15 +0000

    JAVADoc

--- src/bcu/cmp5332/librarysystem/main/CommandParser.java
+++ src/bcu/cmp5332/librarysystem/main/CommandParser.java
@@ -18,47 +18,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * CommandParser class is used to parse the input given by the user through the
+ * application's command line interface. The class has a static method
+ * {@link parse(String) parse(String)} that accepts the input as String and
+ * returns the appropriate
+ * {@link Command} implementation that is meant to execute the command.
+ *
+ * @see parse(String)
+ * @see Command
+ */
 public class CommandParser {
 
-    public static Command parse(String line) throws IOException, LibraryException {
-        try {
-            String[] parts = line.split(" ", 3);
-            String cmd = parts[0];
+	/**
+	 * Get the user input as {@link String} and returns the corresponding
+	 * implementation of
+	 * the {@link Command} interface based on that input. The input represents a
+	 * line and
+	 * contains the command together with the possible arguments that were given by
+	 * the user.
+	 *
+	 * @param line the command given by the user as String
+	 * @return the appropriate implementation of the Command interface
+	 * @throws IOException      if something goes wrong when reading additional
+	 *                          input
+	 * @throws LibraryException if the command given by the user is invalid or does
+	 *                          not exist
+	 */
+	public static Command parse(String line) throws IOException, LibraryException {
+		try {
+			String[] parts = line.split(" ", 3);
+			String cmd = parts[0];
 
-            if (cmd.equals("addbook")) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                System.out.print("Title: ");
-                String title = br.readLine();
-                System.out.print("Author: ");
-                String author = br.readLine();
-                System.out.print("Publisher: ");
-                String publisher = br.readLine();
-                System.out.print("Publication Year: ");
-                String publicationYear = br.readLine();
+			if (cmd.equals("addbook")) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print("Title: ");
+				String title = br.readLine();
+				System.out.print("Author: ");
+				String author = br.readLine();
+				System.out.print("Publisher: ");
+				String publisher = br.readLine();
+				System.out.print("Publication Year: ");
+				String publicationYear = br.readLine();
 
-                return new AddBook(title, author, publisher, publicationYear);
-            } else if (cmd.equals("addpatron")) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                System.out.print("Name: ");
-                String name = br.readLine();
-                System.out.print("Phone: ");
-                String phone = br.readLine();
-                System.out.print("Email: ");
-                String email = br.readLine();
+				return new AddBook(title, author, publisher, publicationYear);
+			} else if (cmd.equals("addpatron")) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print("Name: ");
+				String name = br.readLine();
+				System.out.print("Phone: ");
+				String phone = br.readLine();
+				System.out.print("Email: ");
+				String email = br.readLine();
 
 				return new AddPatron(name, phone, email);
-            } else if (cmd.equals("loadgui")) {
-                return new LoadGUI();
-            } else if (parts.length == 1) {
-                if (line.equals("listbooks")) {
-                    return new ListBooks();
-                } else if (line.equals("listpatrons")) {
-                    return new ListPatrons();
-                } else if (line.equals("help")) {
-                    return new Help();
-                }
-            } else if (parts.length == 2) {
-                int id = Integer.parseInt(parts[1]);
+			} else if (cmd.equals("loadgui")) {
+				return new LoadGUI();
+			} else if (parts.length == 1) {
+				if (line.equals("listbooks")) {
+					return new ListBooks();
+				} else if (line.equals("listpatrons")) {
+					return new ListPatrons();
+				} else if (line.equals("help")) {
+					return new Help();
+				}
+			} else if (parts.length == 2) {
+				int id = Integer.parseInt(parts[1]);
 
 				if (cmd.equals("removebook")) {
 					return new RemoveBook(id);
@@ -66,25 +91,25 @@ public class CommandParser {
 					return new RemovePatron(id);
 				} else if (cmd.equals("showbook")) {
 					return new ShowBook(id);
-                } else if (cmd.equals("showpatron")) {
+				} else if (cmd.equals("showpatron")) {
 					return new ShowPatron(id);
-                }
-            } else if (parts.length == 3) {
-                int patronID = Integer.parseInt(parts[1]);
-                int bookID = Integer.parseInt(parts[2]);
+				}
+			} else if (parts.length == 3) {
+				int patronID = Integer.parseInt(parts[1]);
+				int bookID = Integer.parseInt(parts[2]);
 
-                if (cmd.equals("borrow")) {
+				if (cmd.equals("borrow")) {
 					return new BorrowBook(patronID, bookID);
-                } else if (cmd.equals("renew")) {
+				} else if (cmd.equals("renew")) {
 					return new RenewBook(patronID, bookID);
-                } else if (cmd.equals("return")) {
+				} else if (cmd.equals("return")) {
 					return new ReturnBook(patronID, bookID);
-                }
-            }
-        } catch (NumberFormatException ex) {
+				}
+			}
+		} catch (NumberFormatException ex) {
 
-        }
+		}
 
-        throw new LibraryException("Invalid command.");
-    }
+		throw new LibraryException("Invalid command.");
+	}
 }
