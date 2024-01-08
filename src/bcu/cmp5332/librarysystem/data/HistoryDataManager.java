ea8c0bd9539973001e0c3c7a01de02f17baf3c72
acly7946 <66972514+acly7946@users.noreply.github.com.>
2024-01-08 16:38:57 +0000

    Add datamanger for loan history

---
+++ src/bcu/cmp5332/librarysystem/data/HistoryDataManager.java
@@ -0,0 +1,57 @@
+package bcu.cmp5332.librarysystem.data;
+
+import bcu.cmp5332.librarysystem.model.Library;
+import bcu.cmp5332.librarysystem.model.Loan;
+import bcu.cmp5332.librarysystem.model.Patron;
+import bcu.cmp5332.librarysystem.main.LibraryException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class HistoryDataManager implements DataManager {
+
+    private final String RESOURCE = "./resources/data/history.txt";
+
+    @Override
+    public void loadData(Library library) throws IOException, LibraryException {
+        try (Scanner sc = new Scanner(new File(RESOURCE))) {
+            int line_idx = 1;
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] properties = line.split(SEPARATOR, -1);
+                try {
+                    int patronId = Integer.parseInt(properties[0]);
+					int bookId = Integer.parseInt(properties[1]);
+					LocalDate startDate = LocalDate.parse(properties[2]);
+					LocalDate dueDate = LocalDate.parse(properties[3]);
+					LocalDate returnDate = LocalDate.parse(properties[4]);
+					library.getPatronByID(patronId).addLoanHistory(new Loan(library.getPatronByID(patronId), library.getBookByID(bookId), startDate, dueDate, returnDate));
+                } catch (NumberFormatException ex) {
+                    throw new LibraryException("Unable to parse loan history on line " + line_idx
+                        + "\nError: " + ex);
+                }
+                line_idx++;
+            }
+        }
+    }
+
+    @Override
+    public void storeData(Library library) throws IOException {
+        try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))) {
+            for (Patron patron : library.getPatrons()) {
+				for (Loan loan : patron.getLoanHistory()) {
+					out.println(loan.getPatron().getId() + SEPARATOR);
+					out.println(loan.getBook().getId() + SEPARATOR);
+					out.println(loan.getStartDate() + SEPARATOR);
+					out.println(loan.getDueDate() + SEPARATOR);
+					out.println(loan.getReturnDate() + SEPARATOR);
+				}
+				out.println();
+            }
+        }
+    }
+}
