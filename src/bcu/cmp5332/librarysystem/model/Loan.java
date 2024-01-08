be83262bec71c4f8f4b2f2c23289bbdeeed70b9e
acly7946 <66972514+acly7946@users.noreply.github.com.>
2024-01-08 16:26:01 +0000

    Add returndate to loan class

--- src/bcu/cmp5332/librarysystem/model/Loan.java
+++ src/bcu/cmp5332/librarysystem/model/Loan.java
@@ -8,12 +8,14 @@ public class Loan {
     private Book book;
     private LocalDate startDate;
     private LocalDate dueDate;
+	private LocalDate returnDate;
 
     public Loan(Patron patron, Book book, LocalDate startDate, LocalDate dueDate) {
 		this.patron = patron;
 		this.book = book;
 		this.startDate = startDate;
 		this.dueDate = dueDate;
+		this.returnDate = null;
     }
 
 	public Patron getPatron() {
@@ -46,6 +48,14 @@ public class Loan {
 
 	public void setDueDate(LocalDate dueDate) {
 		this.dueDate = dueDate;
+	}
+
+	public LocalDate getReturnDate() {
+		return this.returnDate;
+	}
+
+	public void setReturnDate(LocalDate returnDate) {
+		this.returnDate = returnDate;
 	}
 
 }
